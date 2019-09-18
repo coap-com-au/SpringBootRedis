f3a756050efe61acf9391055fb7b081555d3d22d
Henry Nguyen <henrylknguyen@gmail.com>
2019-09-18 17:33:02 +1000

    Initial version

---
+++ src/main/java/com/concretepage/dao/FriendDAO.java
@@ -0,0 +1,29 @@
+package com.concretepage.dao;
+
+import javax.annotation.Resource;
+
+import org.springframework.data.redis.core.ListOperations;
+import org.springframework.stereotype.Repository;
+
+import com.concretepage.Person;
+
+@Repository
+public class FriendDAO {
+	 private static final String KEY = "friendsKey";
+
+	 @Resource(name="redisTemplate")
+	 private ListOperations<String, Person> opsForList;	 	 
+	  
+	 public void addFriend(Person person) {
+		 opsForList.leftPush(KEY, person);
+	 }
+	 public long getNumberOfFriends() {
+		 return opsForList.size(KEY);
+	 }
+	 public Person getFriendAtIndex(Integer index) {
+		 return opsForList.index(KEY, index);
+	 }
+	 public void removeFriend(Person p) {
+		 opsForList.remove(KEY, 1, p);
+	 }
+}
